d924fccf5e1c521b7cc4b226f73d3fb449ad0c9e
hnluu <hung.luu@loominus.ai>
2019-12-30 11:36:07 -0800

    Initial commit to test CI

--- pdfner/tests/test_search.py
+++ pdfner/tests/test_search.py
@@ -13,13 +13,24 @@ container = IocContainer(
 
 class TestSearch:
     def test_index(self):
-        doc = NerDocument(title='Hello, World!')
+        doc = NerDocument(title='A document to test with',
+                          document_name='test')
         es_client: ElasticsearchClient = container.elasticsearch_client()
         es_client.index(doc)
 
-    def test_search(self):
+    def test_search_by_single_field(self):
         es_client: ElasticsearchClient = container.elasticsearch_client()
         s = NerDocument.search(using=es_client.es)
-        s = s.query('match', title='World')
+        s = s.query('match', title='test')
+        result = s.execute()
+        print(result)
+
+    def test_search_by_multiple_fields(self):
+        es_client: ElasticsearchClient = container.elasticsearch_client()
+        s = NerDocument.search(using=es_client.es)
+        s = s.highlight_options(order='score')
+        s = s.highlight('title')
+        s = s.highlight('document_name')
+        s = s.query('multi_match', query='test', fields=['document_name', 'title'], type='best_fields')
         result = s.execute()
         print(result)
